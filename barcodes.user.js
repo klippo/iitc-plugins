107b30118730d892d3d19d430e1b68f940c96338
Jack Miner <3ch01c@gmail.com>
2016-07-24 13:52:29 -0600

    Updated change date on barcodes

--- barcodes.user.js
+++ barcodes.user.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-barcodes@3ch01c
 // @name           IITC plugin: Replace player names with more easily remembered names
 // @category       Portal Info
-// @version        0.0.1.201606231553
+// @version        0.0.1.201607241351
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/3ch01c/iitc-plugins/raw/master/barcodes.user.js
 // @downloadURL    https://github.com/3ch01c/iitc-plugins/raw/master/barcodes.user.js
-// @description    [local-2016-06-23-155300] Show resonator energy percentage on resonator energy bar in portal detail panel.
+// @description    [local-2016-07-24-135100] Show resonator energy percentage on resonator energy bar in portal detail panel.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
